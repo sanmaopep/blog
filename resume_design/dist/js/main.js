c9adc8697a03096fb5f34254b8260ccfce91a9aa
maoyiwei <jsmyw95@126.com>
2017-11-27 17:32:58 +0800

    优化细节

--- resume_design/dist/js/main.js
+++ resume_design/dist/js/main.js
@@ -42,54 +42,13 @@ function storyState() {
         this.stories[i].style.visibility = "hidden";
         this.stories[i].style.display = "none";
         this.stories[i].style.opacity = "0";
+        this.stories[i].style.position = "relative";
+        this.stories[i].style.left = "-30px";
         // 在CSS中添加transition属性
     }
 
-    // 设置currIndex
-    Object.defineProperty(this, 'currIndex', {
-        set(index) {
-            // 保护锁
-            if (this.setCurrIndexBlock) {
-                return;
-            }
-            if (typeof index === "number") {
-                if (this.curr === index) {
-                    this.stories[index].style.display = "block";
-
-                    this.setCurrIndexBlock = true;
-                    setTimeout(() => {
-                        this.stories[index].style.visibility = "visible";
-                        this.stories[index].style.opacity = "1";
-                        this.setCurrIndexBlock = false;
-                    }, 250);
-                } else {
-                    // 隐藏
-                    this.stories[this.curr].style.visibility = "hidden";
-                    this.stories[this.curr].style.opacity = "0";
-
-                    this.setCurrIndexBlock = true;
-                    setTimeout(() => {
-                        // 隐藏
-                        this.stories[this.curr].style.display = "none";
-                        this.stories[index].style.display = "block";
-                        setTimeout(() => {
-                            this.stories[index].style.visibility = "visible";
-                            this.stories[index].style.opacity = "1";
-
-                            this.setCurrIndexBlock = false;
-                            this.curr = index;
-                        }, 250);
-                    }, 250);
-                }
-            }
-        },
-        get() {
-            return this.curr;
-        }
-    });
-
     this.curr = 0;
-    this.currIndex = 0;
+    this._setCurrIndex(0);
 
     // 滚轮事件
     this.storyOutterDOM.addEventListener('mousewheel', event => {
@@ -114,17 +73,66 @@ function storyState() {
     // TODO 移动端touch事件
 }
 
+storyState.prototype._setCurrIndex = function (index, cb) {
+    // 保护锁
+    if (this.setCurrIndexBlock) {
+        return;
+    }
+    if (typeof index === "number") {
+        if (this.curr === index) {
+            this.stories[index].style.display = "block";
+            this.setCurrIndexBlock = true;
+            setTimeout(() => {
+                this.stories[index].style.visibility = "visible";
+                this.stories[index].style.opacity = "1";
+                this.stories[index].style.left = "0px";
+                this.setCurrIndexBlock = false;
+            }, 250);
+        } else {
+            // 隐藏
+            this.stories[this.curr].style.visibility = "hidden";
+            this.stories[this.curr].style.opacity = "0";
+            this.stories[this.curr].style.left = "-30px";
+
+            this.setCurrIndexBlock = true;
+            setTimeout(() => {
+                // 隐藏
+                this.stories[this.curr].style.display = "none";
+                this.stories[index].style.display = "block";
+                setTimeout(() => {
+                    this.stories[index].style.visibility = "visible";
+                    this.stories[index].style.opacity = "1";
+                    this.stories[index].style.left = "0px";
+                    this.setCurrIndexBlock = false;
+                    this.curr = index;
+                    cb();
+                }, 250);
+            }, 250);
+        }
+    }
+};
+
+storyState.prototype._getCurrIndex = function (index) {
+    return this.curr;
+};
+
 storyState.prototype._goStory = function (index) {};
 
 storyState.prototype._upStory = function () {
-    if (this.currIndex > 0) {
-        this.currIndex = this.currIndex - 1;
+    if (this._getCurrIndex() > 0) {
+        this._setCurrIndex(this._getCurrIndex() - 1, () => {
+            let offsetHeight = this.storyOutterDOM.offsetHeight;
+            let scrollHeight = this.storyOutterDOM.scrollHeight;
+            this.storyOutterDOM.scrollTop = scrollHeight - offsetHeight;
+        });
     }
 };
 
 storyState.prototype._downStory = function () {
-    if (this.currIndex < this.storiesLen - 1) {
-        this.currIndex = this.currIndex + 1;
+    if (this._getCurrIndex() < this.storiesLen - 1) {
+        this._setCurrIndex(this._getCurrIndex() + 1, () => {
+            this.storyOutterDOM.scrollTop = 0;
+        });
     }
 };
 
